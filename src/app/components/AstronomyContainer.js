ad7ab8ef11f90be8b610ebfb8275493df46c4959
Brian Sullivan <bri.g.sullivan@gmail.com>
2017-09-02 10:03:02 -0400

    redux for state management - removed API Key

--- src/app/components/AstronomyContainer.js
+++ src/app/components/AstronomyContainer.js
@@ -1,41 +1,23 @@
 import React, { Component } from 'react'
 import AstronomyCard from './AstronomyCard'
-import axios from 'axios'
+import { connect } from 'react-redux'
+import fetchData from '../../actions/fetch_data'
 
 class AstronomyContainer extends Component {
-    
-    constructor() {
-        super()
 
-        this.state = {
-            astronomy: []
-        }
+    componentWillMount() {
+        this.props.fetchData()
     }
 
-    componentDidMount() {
-        const API_KEY = 'xf4SbKw7ddKNqMd6Pi81oZaR4vbYciuW8yLQhmfq'
-        const END_POINT = 'https://api.nasa.gov/planetary/apod?api_key='
-
-        axios.get(END_POINT+API_KEY)
-            .then(response => {
-                
-                this.setState({
-                    astronomy: response.data
-                })
-                
-                console.log(this.state.astronomy)
-            })
-            .catch(error => {
-                console.log(error, 'failed to fetch data')
-            })
-    }
-    
     render() {
-        const { astronomy } = this.state
         return (
-            <AstronomyCard data={astronomy} />
+            <AstronomyCard data={this.props.astronomy} />
         )
     }
 }
 
-export default AstronomyContainer
+function mapStateToProps(state) {
+    return { astronomy: state.astronomy }
+}
+
+export default connect(mapStateToProps, { fetchData })(AstronomyContainer)
--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ require('./assets/stylesheets/styles.styl')
 
 import React from 'react';
 import ReactDOM from 'react-dom'
-import App from './app/App.js'
+import { Provider } from 'react-redux'
+import Store from './Store'
+import App from './app/App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+    <Provider store={Store}> 
+        <App />
+    </Provider>,
+    document.getElementById('root')
+)
